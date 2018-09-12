d1219f370a570f25dee6fa707924485df11d170d
Tobuss <33454600+Tobuss@users.noreply.github.com>
2018-09-12 20:37:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,23 +5,40 @@ const fs = require("fs");
 const bot = new Discord.Client({DisableEveryone: true});
 bot.commands = new Discord.Collection();
 const embed = require("./Schedule/boss.js")
-const Enmap = require('enmap');
-const EnmapLevel = require('enmap-level');
-const sql = require('better-sqlite-pool');
-const myEnmap = new Enmap({name: "users"});
-// non-cached, auto-fetch enmap: 
-const otherEnmap = new Enmap({
-  name: "settings",
-  autoFetch: true,
-  fetchAll: false
-});
+// const Enmap = require("enmap");
+// const EnmapLevel = require('enmap-level');
+// const sql = require('better-sqlite-pool');
+// //const myEnmap = new Enmap({name: "users"});
+// // non-cached, auto-fetch enmap: 
+// const myEnmap = new Enmap({
+//   name: "user",
+//   persistent: true,
+//   autoFetch: true,
+//   fetchAll: true
+// });
+// myEnmap.set("userar", {
+//   ids: "",
+  
+// });
+// myEnmap.defer.then( () => {
+//     console.log(myEnmap.size + " keys loaded");
+//   });
+  
+// module.exports = {
+//   myEnmap
+// }
 
-myEnmap.defer.then( () => {
-    console.log(myEnmap.size + " keys loaded");
-  });
 
-  const talkedRecently = new Set();
+// bot.on("ready", () => {
 
+//   const fetch = myEnmap.fetchEverything();
+//   for (let i = 0; i < fetch.length; i++) {
+//     console.log(fetch[i]);
+//   }
+   
+//   // console.log(fetch);
+//   // bot.users.get(fetch).send("someMessage");
+// })
 
 
 bot.on("ready", async () => {
@@ -166,267 +183,288 @@ scheduled: true,
 timeZone: 'Europe/Athens'
 });
 //     //Karanda
-    
-//     var Karanda1 = new schedule.RecurrenceRule();
-//     Karanda1.dayOfWeek = [2,6];
-//     Karanda1.hour = 0
-//     Karanda1.minute = 5 
-//     var j = schedule.scheduleJob(Karanda1, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: karanda});
-//         console.log('Boss Spawn')
-//     });
+var Karanda1 = cron.schedule('05 00 * * 2,6', () =>  {
+  let Karanda1 = require('./Schedule/karanda.js');
+  bot.channels.get("422437762893873172").send({embed: karanda});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Karanda2 = cron.schedule('50 01 * * 1,3', () =>  {
+  let Karanda2 = require('./Schedule/karanda.js');
+  bot.channels.get("422437762893873172").send({embed: karanda});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
         
-//     var Karanda2 = new schedule.RecurrenceRule();
-//     Karanda2.dayOfWeek = [1,3];
-//     Karanda2.hour = 1
-//     Karanda2.minute = 50 
-//     var j = schedule.scheduleJob(Karanda2, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: karanda});
-//         console.log('Boss Spawn')
-    
-//    }); 
-//     var Karanda3 = new schedule.RecurrenceRule();
-//     Karanda3.dayOfWeek = [5];
-//     Karanda3.hour = 4
-//     Karanda3.minute = 50 
-//     var j = schedule.scheduleJob(kutum3, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: karanda});
-//         console.log('Boss Spawn')
-//     });
-//     var Karanda4 = new schedule.RecurrenceRule();
-//     Karanda4.dayOfWeek = [3];
-//     Karanda4.hour = 8
-//     Karanda4.minute = 50 
-//     var j = schedule.scheduleJob(Karanda4, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: karanda});
-//         console.log('Boss Spawn')
-//     });
-//     var Karanda5 = new schedule.RecurrenceRule();
-//     Karanda5.dayOfWeek = [5];
-//     Karanda5.hour = 11
-//     Karanda5.minute = 50 
-//     var j = schedule.scheduleJob(Karanda5, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: karanda});
-//         console.log('Boss Spawn')
-//     });
-//     var Karanda6 = new schedule.RecurrenceRule();
-//     Karanda6.dayOfWeek = [0,2,6];
-//     Karanda6.hour = 18
-//     Karanda6.minute = 50 
-//     var j = schedule.scheduleJob(Karanda6, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: karanda});
-//         console.log('Boss Spawn')
-//     });
-//     var Karanda7 = new schedule.RecurrenceRule();
-//     Karanda7.dayOfWeek = [3,4];
-//     Karanda7.hour = 22
-//     Karanda7.minute = 5 
-//     var j = schedule.scheduleJob(Karanda7, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: karanda});
-//         console.log('Boss Spawn')
-// });
+var Karanda3 = cron.schedule('50 04 * * 5', () =>  {
+  let Karanda3 = require('./Schedule/karanda.js');
+  bot.channels.get("422437762893873172").send({embed: karanda});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Karanda4 = cron.schedule('50 08 * * 3', () =>  {
+  let Karanda4 = require('./Schedule/karanda.js');
+  bot.channels.get("422437762893873172").send({embed: karanda});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Karanda5 = cron.schedule('50 11 * * 5', () =>  {
+  let Karanda5 = require('./Schedule/karanda.js');
+  bot.channels.get("422437762893873172").send({embed: karanda});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Karanda6 = cron.schedule('50 18 * * 0,2,6', () =>  {
+  let Karanda6 = require('./Schedule/karanda.js');
+  bot.channels.get("422437762893873172").send({embed: karanda});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Karanda7 = cron.schedule('05 22 * * 3,4', () =>  {
+  let Karanda7 = require('./Schedule/karanda.js');
+  bot.channels.get("422437762893873172").send({embed: karanda});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
 //     //Kzarka
-    
-//     var Kzarka1 = new schedule.RecurrenceRule();
-//     Kzarka1.dayOfWeek = [5];
-//     Kzarka1.hour = 0
-//     Kzarka1.minute = 5 
-//     var j = schedule.scheduleJob(Kzarka1, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Kzarka});
-//         console.log('Boss Spawn')
-//     });
+var Kzarka1 = cron.schedule('05 00 * * 5', () =>  {
+  let Kzarka1 = require('./Schedule/Kzarka.js');
+  bot.channels.get("422437762893873172").send({embed: kzarka});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Kzarka2 = cron.schedule('50 1 * * 0', () =>  {
+  let Kzarka2 = require('./Schedule/Kzarka.js');
+  bot.channels.get("422437762893873172").send({embed: kzarka});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
         
-//     var Kzarka2 = new schedule.RecurrenceRule();
-//     Kzarka2.dayOfWeek = [0];
-//     Kzarka2.hour = 1
-//     Kzarka2.minute = 50 
-//     var j = schedule.scheduleJob(Kzarka2, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Kzarka});
-//         console.log('Boss Spawn')
-//      }); 
+var Kzarka3 = cron.schedule('50 4 * * 1,2,3', () =>  {
+  let Kzarka3 = require('./Schedule/Kzarka.js');
+  bot.channels.get("422437762893873172").send({embed: kzarka});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
      
-//     var Kzarka3 = new schedule.RecurrenceRule();
-//     Kzarka3.dayOfWeek = [1,2,3];
-//     Kzarka3.hour = 4
-//     Kzarka3.minute = 50 
-//     var j = schedule.scheduleJob(Kzarka3, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Kzarka});
-//         console.log('Boss Spawn')
-//     });
-//     var Kzarka4 = new schedule.RecurrenceRule();
-//     Kzarka4.dayOfWeek = [1];
-//     Kzarka4.hour = 8
-//     Kzarka4.minute = 50 
-//     var j = schedule.scheduleJob(Kzarka4, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Kzarka});
-//         console.log('Boss Spawn')
-//     });
-//     var Kzarka5 = new schedule.RecurrenceRule();
-//     Kzarka5.dayOfWeek = [0];
-//     Kzarka5.hour = 11
-//     Kzarka5.minute = 50 
-//     var j = schedule.scheduleJob(Kzarka5, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Kzarka});
-//         console.log('Boss Spawn')
-//     });
-//     var Kzarka6 = new schedule.RecurrenceRule();
-//     Kzarka6.dayOfWeek = [3];
-//     Kzarka6.hour = 15
-//     Kzarka6.minute = 50 
-//     var j = schedule.scheduleJob(Kzarka6, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Kzarka});
-//         console.log('Boss Spawn')
-//     });
-//     var Kzarka7 = new schedule.RecurrenceRule();
-//     Kzarka7.dayOfWeek = [5,6];
-//     Kzarka7.hour = 18
-//     Kzarka7.minute = 50
-//     var j = schedule.scheduleJob(Kzarka7, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Kzarka});
-//         console.log('Boss Spawn')
-//     });
-//     var Kzarka8 = new schedule.RecurrenceRule();
-//     Kzarka8.dayOfWeek = [1,2,3,5,7];
-//     Kzarka8.hour = 22
-//     Kzarka8.minute = 5
-//     var j = schedule.scheduleJob(Kzarka8, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Kzarka});
-//         console.log('Boss Spawn')
-//     });
+var Kzarka4 = cron.schedule('50 8 * * 1', () =>  {
+  let Kzarka4 = require('./Schedule/Kzarka.js');
+  bot.channels.get("422437762893873172").send({embed: kzarka});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Kzarka5 = cron.schedule('50 11 * * 0', () =>  {
+  let Kzarka5 = require('./Schedule/Kzarka.js');
+  bot.channels.get("422437762893873172").send({embed: kzarka});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Kzarka6 = cron.schedule('50 15 * * 3', () =>  {
+  let Kzarka6 = require('./Schedule/Kzarka.js');
+  bot.channels.get("422437762893873172").send({embed: kzarka});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Kzarka7 = cron.schedule('50 18 * * 5,6', () =>  {
+  let Kzarka7 = require('./Schedule/Kzarka.js');
+  bot.channels.get("422437762893873172").send({embed: kzarka});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Kzarka8 = cron.schedule('5 22 * * 1,2,3,5,7', () =>  {
+  let Kzarka8 = require('./Schedule/Kzarka.js');
+  bot.channels.get("422437762893873172").send({embed: kzarka});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+
 //     //Nouver
-    
-//     var Nouver1 = new schedule.RecurrenceRule();
-//     Nouver1.dayOfWeek = [4,7];
-//     Nouver1.hour = 0
-//     Nouver1.minute = 5 
-//     var j = schedule.scheduleJob(Nouver1, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Nouver});
-//         console.log('Boss Spawn')
-//     });
+var Nouver1 = cron.schedule('0 5 * * 4,7', () =>  {
+  let Nouver1 = require('./Schedule/Nouver.js');
+  bot.channels.get("422437762893873172").send({embed: Nouver});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Nouver2 = cron.schedule('50 1 * * 5', () =>  {
+  let Nouver2 = require('./Schedule/Nouver.js');
+  bot.channels.get("422437762893873172").send({embed: Nouver});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
         
-//     var Nouver2 = new schedule.RecurrenceRule();
-//     Nouver2.dayOfWeek = [5];
-//     Nouver2.hour = 1
-//     Nouver2.minute = 50 
-//     var j = schedule.scheduleJob(Nouver2, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Nouver});
-//         console.log('Boss Spawn')
-    
-//    }); 
-//     var Nouver3 = new schedule.RecurrenceRule();
-//     Nouver3.dayOfWeek = [4,6];
-//     Nouver3.hour = 4
-//     Nouver3.minute = 50 
-//     var j = schedule.scheduleJob(Nouver3, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Nouver});
-//         console.log('Boss Spawn')
-//     });
-//     var Nouver4 = new schedule.RecurrenceRule();
-//     Nouver4.dayOfWeek = [0];
-//     Nouver4.hour = 8
-//     Nouver4.minute = 50 
-//     var j = schedule.scheduleJob(Nouver4, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Nouver});
-//         console.log('Boss Spawn')
-//     });
-//     var Nouver5 = new schedule.RecurrenceRule();
-//     Nouver5.dayOfWeek = [1,4,6];
-//     Nouver5.hour = 11
-//     Nouver5.minute = 50 
-//     var j = schedule.scheduleJob(Nouver5, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Nouver});
-//         console.log('Boss Spawn')
-//     });
-//     var Nouver6 = new schedule.RecurrenceRule();
-//     Nouver6.dayOfWeek = [2,5];
-//     Nouver6.hour = 15
-//     Nouver6.minute = 50 
-//     var j = schedule.scheduleJob(Nouver6, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Nouver});
-//         console.log('Boss Spawn')
-//     });
-//     var Nouver7 = new schedule.RecurrenceRule();
-//     Nouver7.dayOfWeek = [1];
-//     Nouver7.hour = 18
-//     Nouver7.minute = 50 
-//     var j = schedule.scheduleJob(Nouver7, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Nouver});
-//         console.log('Boss Spawn')
-//     });
-//     var Nouver8 = new schedule.RecurrenceRule();
-//     Nouver8.dayOfWeek = [0,2];
-//     Nouver8.hour = 22
-//     Nouver8.minute = 5 
-//     var j = schedule.scheduleJob(Nouver8, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Nouver});
-//         console.log('Boss Spawn')
-//     });
+var Nouver3 = cron.schedule('50 4 * * 4,6', () =>  {
+  let Nouver3 = require('./Schedule/Nouver.js');
+  bot.channels.get("422437762893873172").send({embed: Nouver});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Nouver4 = cron.schedule('50 8 * * 0', () =>  {
+  let Nouver4 = require('./Schedule/Nouver.js');
+  bot.channels.get("422437762893873172").send({embed: Nouver});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Nouver5 = cron.schedule('50 11 * * 1,4,6', () =>  {
+  let Nouver5 = require('./Schedule/Nouver.js');
+  bot.channels.get("422437762893873172").send({embed: Nouver});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Nouver6 = cron.schedule('50 15 * * 2,5', () =>  {
+  let Nouver6 = require('./Schedule/Nouver.js');
+  bot.channels.get("422437762893873172").send({embed: Nouver});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Nouver7 = cron.schedule('50 18 * * 1', () =>  {
+  let Nouver7 = require('./Schedule/Nouver.js');
+  bot.channels.get("422437762893873172").send({embed: Nouver});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+var Nouver8 = cron.schedule('5 22 * * 0,2', () =>  {
+  let Nouver8 = require('./Schedule/Nouver.js');
+  bot.channels.get("422437762893873172").send({embed: Nouver});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});
+
 //     //Offin
-    
-//     var Offin1 = new schedule.RecurrenceRule();
-//     Offin1.dayOfWeek = [6];
-//     Offin1.hour = 1
-//     Offin1.minute = 50 
-//     var j = schedule.scheduleJob(Offin1, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Offin});
-//         console.log('Boss Spawn')
-//     });
+var Offin1 = cron.schedule('50 1 * * 6', () =>  {
+  let Offin1 = require('./Schedule/Offin.js');
+  bot.channels.get("422437762893873172").send({embed: Offin});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});   
+var Offin2 = cron.schedule('50 11 * * 2', () =>  {
+  let Offin2 = require('./Schedule/Offin.js');
+  bot.channels.get("422437762893873172").send({embed: Offin});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});   
         
-//     var Offin2 = new schedule.RecurrenceRule();
-//     Offin2.dayOfWeek = [2];
-//     Offin2.hour = 11
-//     Offin2.minute = 50 
-//     var j = schedule.scheduleJob(Offin2, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Offin});
-//         console.log('Boss Spawn')
-    
-//    }); 
-//     var Offin3 = new schedule.RecurrenceRule();
-//     Offin3.dayOfWeek = [4];
-//     Offin3.hour = 18
-//     Offin3.minute = 50 
-//     var j = schedule.scheduleJob(Offin3, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Offin});
-//         console.log('Boss Spawn')
-//     });
-    
+var Offin3 = cron.schedule('50 18 * * 4', () =>  {
+  let Offin3 = require('./Schedule/Offin.js');
+  bot.channels.get("422437762893873172").send({embed: Offin});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});   
+
 //     //Vell
-    
-//     var Vell1 = new schedule.RecurrenceRule();
-//     Vell1.dayOfWeek = [7];
-//     Vell1.hour = 15
-//     Vell1.minute = 50
-//     var j = schedule.scheduleJob(Vell1, function(){
-//         let kutum = require('./Schedule/boss.js');
-//         bot.channels.get("422437762893873172").send({embed: Vell});
-//         console.log('Boss Spawn')
-// });
+var Vell = cron.schedule('50 15 * * 7', () =>  {
+  let Vell = require('./Schedule/Vell.js');
+  bot.channels.get("422437762893873172").send({embed: Vell});
+  console.log('Boss Spawn');
+}, {
+
+start: true,
+scheduled: true,
+timeZone: 'Europe/Athens'
+});   
       console.log('The answer to life, the universe, and everything!');
 
 
