31d314fae0c5ee34dc57206f6401967bc9f28f1c
Tobuss <33454600+Tobuss@users.noreply.github.com>
2018-09-13 12:20:01 +0100

    Add files via upload

--- commands/subscribe.js
+++ commands/subscribe.js
@@ -1,49 +1,45 @@
-const Discord = require("discord.js"); 
-var client = new Discord.Client
-const Enmap = require('enmap');
-const EnmapLevel = require('enmap-level');
-const sql = require('better-sqlite-pool');
-const myEnmap = new Enmap({name: "users"});
-var stringify = require('json-stringify-safe');
+// const Discord = require("discord.js"); 
+// var client = new Discord.Client
+// const Enmap = require('enmap');
+// const EnmapLevel = require('enmap-level');
+// const sql = require('better-sqlite-pool');
+// //const myEnmap = new Enmap({name: "users"});
+// const index = require("./../index.js");
+// let myEnmap = index.myEnmap
+// var stringify = require('json-stringify-safe');
+// let cooldown = new Set()
+// let cdseconds = 5;
 
 
-
-exports.run = (client, message, args) => {
-
-    if (talkedRecently.has(msg.author.id)) {
-        msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
-} else {
-
-       // the user can type the command ... your command code goes here :)
-
-    // Adds the user to the set so that they can't talk for a minute
-    talkedRecently.add(msg.author.id);
-    setTimeout(() => {
-      // Removes the user from the set after a minute
-      talkedRecently.delete(msg.author.id);
-    }, 60000);
-}
-
-    // let user = message.mentions.user.first();
-    // channel.send(user);
-    message.guild.fetchMember(message.author.id)
-    .then(member => {
-        console.log(message.author.id)
-        try {
-            myEnmap.set("user", message.author.id);
-          }
-          catch(error) {
-            console.error(error);
-            // expected output: SyntaxError: unterminated string literal
-            // Note - error messages will vary depending on browser
-          }
-        message.author.send("hello");
-    });
+//     exports.run = (client, message, args) => {
+//         if(!message.content.startsWith("!")) return;
+//         if(cooldown.has(message.author.id)){
+//             message.delete();
+//            return message.reply("One time use command boi!")
+//         }
+//         cooldown.add(message.author.id);
+//         message.guild.fetchMember(message.author.id)
+//      .then(member => {
+//          console.log(message.author.id)
+//          try {
+//              myEnmap.set(message.author.id, message.author.id);
+//            }
+//            catch(error) {
+//              console.error(error);
+//              // expected output: SyntaxError: unterminated string literal
+//              // Note - error messages will vary depending on browser
+//            }
+       
+//        message.author.send("hello");
+       
+//      });
+//     setTimeout(() => {
+//         cooldown.delete(message.author.id)
+//         }, cdseconds * 10000000 )
+// }
 
 
+//  module.exports.subscribe = {
 
-}
- module.exports.subscribe = {
-
-     name: "subscribe"
- }
+//      name: "subscribe"
+//  }
